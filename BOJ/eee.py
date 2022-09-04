55982af0e6b63f7f0743ef3fed829d67e6097d46
98-jeonghoon <bjh4395@naver.com>
2022-09-05 03:07:32 +0900

    first commit

--- BOJ/eee.py
+++ BOJ/eee.py
@@ -1,9 +1,4 @@
-def even(items):
-    result = []
-    for item in items:
-        if item % 2 == 0:
-            result.append(item)
-    return result
+a = 1
+b = 3
 
-a = list(map(int, input().split()))
-print(a)
+print(a+b)
