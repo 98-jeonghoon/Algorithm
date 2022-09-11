537803b83d5ced5cfb5380e1aac7ba0059418dd1
98-jeonghoon <bjh4395@naver.com>
2022-09-12 03:19:03 +0900

    BOJ 2606

--- ex.py
+++ ex.py
@@ -619,41 +619,42 @@
 # print(result)
 
 from collections import deque
+import queue
 
-n, m, v = map(int, input().split())
+n = int(input())
+m = int(input())
 graph = [[] for _ in range(n+1)]
-
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
     
-for i in range(1, n+1):
+for i in range(n+1):
     graph[i].sort()
     
+count = 0
 visited = [False] * (n+1)
+
 def dfs(graph, v, visited):
+    global count
     visited[v] = True
-    print(v, end=' ')
     for i in graph[v]:
         if not visited[i]:
+            count += 1
             dfs(graph, i, visited)
-dfs(graph, v, visited)
-
-print()
-
-visited = [False] * (n+1)
+            
 def bfs(graph, v, visited):
+    global count
     queue = deque([v])
     visited[v] = True
     while queue:
         now = queue.popleft()
-        print(now, end=' ')
         for i in graph[now]:
             if not visited[i]:
+                count += 1
                 queue.append(i)
                 visited[i] = True
-    
-bfs(graph, v, visited)
-        
-        
+                
+# dfs(graph, 1, visited)
+bfs(graph, 1, visited)
+print(count)
