407f032f90f8d11bad4483006d0353cfa696f1d1
98-jeonghoon <bjh4395@naver.com>
2022-09-15 02:22:58 +0900

    정렬 알고리즘

--- ex.py
+++ ex.py
@@ -1,7 +1,8 @@
-from collections import deque
-    
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# from collections import deque
+# import sys
+# sys.setrecursionlimit(10**6)
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
 
 # def bfs(x,y):
 #     queue = deque()
@@ -20,31 +21,35 @@ dy = [0, 0, -1, 1]
 #                 queue.append((nx, ny))
 
 
-def dfs(x,y):
-    if x < 0 or y < 0 or x >= n or y >= m:
-        return False
-    if graph[x][y] == 1:
-        graph[x][y] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny)
-        return True
-    return False
+# def dfs(x,y):
+#     if x < 0 or y < 0 or x >= n or y >= m:
+#         return False
+#     if graph[x][y] == 1:
+#         graph[x][y] = 0
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             dfs(nx, ny)
+#         return True
+#     return False
 
                 
-t = int(input())
-for _ in range(t):
-    m, n, k = map(int, input().split())
-    count = 0
-    graph = [[0]*m for _ in range(n)]
+# t = int(input())
+# for _ in range(t):
+#     m, n, k = map(int, input().split())
+#     count = 0
+#     graph = [[0]*m for _ in range(n)]
 
-    for _ in range(k):
-        a, b = map(int , input().split())
-        graph[b][a] = 1
+#     for _ in range(k):
+#         a, b = map(int , input().split())
+#         graph[b][a] = 1
     
-    for i in range(n):
-        for j in range(m):
-            if dfs(i,j) == True:
-                count += 1
-    print(count)
+#     for i in range(n):
+#         for j in range(m):
+#             if dfs(i,j) == True:
+#                 count += 1
+#     print(count)
+
+a = input()
+list = list(a)
+print(type(int(a[1])))
