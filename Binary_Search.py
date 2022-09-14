289c49d3e3eb9eeda40b326ca20c0dc441026e89
98-jeonghoon <bjh4395@naver.com>
2022-09-15 07:38:34 +0900

    순차 탐색, 이진 탐색

---
+++ Binary_Search.py
@@ -0,0 +1,42 @@
+# 재귀를 이용한 구현
+# def binary_search(array, target, start, end):
+#     if start > end:
+#         return None
+#     mid = (start + end) // 2
+#     if array[mid] == target:
+#         return mid
+#     elif array[mid] > target:
+#         return binary_search(array, target, start, mid - 1)
+#     else:
+#         return binary_search(array, target, mid + 1, end)
+    
+# n, target = list(map(int, input().split()))
+# array = list(map(int, input().split()))
+
+# result = binary_search(array, target, 0, n-1)
+# if result == None:
+#     print('No data')
+# else:
+#     print(result + 1)
+
+# 반복문을 이용한 구현
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid -1
+        else:
+            start = mid + 1
+    return None
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print('No data')
+else:
+    print(result + 1)
+        
