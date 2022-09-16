acfe701e5c2495f9de8b6f93cd07c8774b065e9e
98-jeonghoon <bjh4395@naver.com>
2022-09-17 04:27:35 +0900

    부품 찾기

---
+++ 부품 찾기.py
@@ -0,0 +1,49 @@
+# n = int(input())
+# array = list(map(int, input().split()))
+# m = int(input())
+# guest = list(map(int, input().split()))
+
+# def binary_search(array, target, start, end):
+#     while start <= end:
+#         mid = (start +  end) // 2
+#         if array[mid] == target:
+#             return mid
+#         elif array[mid] > target:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
+#     return None
+
+# for i in guest:
+#     result = binary_search(array, i, 0, n - 1)
+#     if result != None:
+#         print('yes', end=' ')
+#     else:
+#         print('no', end=' ')
+
+# n = int(input())
+# array = [0] * 1000001
+
+# for i in input().split():
+#     array[int(i)] = 1
+    
+# m = int(input())
+# x = list(map(int, input().split()))
+
+# for i in x:
+#     if array[i] == 1:
+#         print('yes', end=' ')
+#     else:
+#         print('no', end=' ')
+
+n = int(input())
+array = set(map(int, input().split()))
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i in array:
+        print('yse', end=' ')
+    else:
+        print('no', end=' ')
+        
