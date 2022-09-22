d6a883728bb43baa3f82dddf16cc17b623466a77
98-jeonghoon <bjh4395@naver.com>
2022-09-22 12:38:12 +0900

    그리디 문제 풀이

---
+++ 이코테 연습문제/모험가 길드.py
@@ -0,0 +1,15 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0 # 총 그룹수
+count = 0 # 해당 그룹에 인원수
+
+# 1 2 2 2 3
+for i in data:
+    count += 1
+    if count <= i:
+        result += 1
+        count = 0
+        
+print(result)
