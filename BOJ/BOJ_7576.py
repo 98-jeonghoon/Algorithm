2b99c8f81163c4716e70b0fd449671f33c58ad12
98-jeonghoon <bjh4395@naver.com>
2022-09-24 17:01:40 +0900

    백준 7576

--- BOJ/BOJ_7576.py
+++ BOJ/BOJ_7576.py
@@ -1,12 +1,44 @@
 from collections import deque
-m , n = map(int, input().split())
-graph = [list(map(int, input().split())) for i in range(n)]
+
+m, n = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
 queue = deque()
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
-            queue.append((i,j))
-
+            queue.append((i, j))
+    
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == -1:
+                continue
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+        
+bfs()
+
+flag = False
+days = 0
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 0:
+            flag = True
+        days = max(days, graph[i][j])
+        
+if flag:
+    print(-1)
+else:
+    print(days-1)
