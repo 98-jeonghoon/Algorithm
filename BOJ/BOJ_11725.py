1a96f64097472d2bc8f6344258fde4b600305d9f
98-jeonghoon <bjh4395@naver.com>
2022-09-29 23:27:00 +0900

    백준 11725

--- BOJ/BOJ_11725.py
+++ BOJ/BOJ_11725.py
@@ -1,3 +1,8 @@
+import sys
+from collections import deque
+
+sys.setrecursionlimit(10**6)
+
 n = int(input())
 graph = [[] for _ in range(n+1)]
 for _ in range(n-1):
@@ -7,13 +12,24 @@ for _ in range(n-1):
     
 visited = [0] * (n + 1)
 
-def dfs(n):
-    for i in graph[n]:
-        if visited[i] == 0:
-            visited[i] = n
-            dfs(i)
-            
-dfs(1)
+# def dfs(n):
+#     for i in graph[n]:
+#         if visited[i] == 0:
+#             visited[i] = n
+#             dfs(i)
 
+# dfs(1)
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if visited[i] == 0:
+                visited[i] = v
+                queue.append(i)
+        
+bfs(1)                    
 for i in range(2, n + 1):
     print(visited[i])
+    
