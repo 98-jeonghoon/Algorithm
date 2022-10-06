a065503e721e7f80064f6fba9cae4a3c0936a908
98-jeonghoon <bjh4395@naver.com>
2022-10-06 18:09:18 +0900

    백준 연구소 다시풀기

--- ex.py
+++ ex.py
@@ -287,16 +287,179 @@
     
 # print(days-1)
 
+# from collections import deque
+
+# n, m = map(int, input().split())
+
+# graph = [[] for _ in range(n+1)]
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+    
+# for i in range(n+1):
+#     graph[i].sort()
+    
+# visited = [False] * (n+1)
+
+# def bfs(graph, v, visited):
+#     queue = deque()
+#     queue.append(v)
+#     visited[v] = True
+#     while queue:
+#         now = queue.popleft()
+#         for i in graph[now]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i] = True
+                
+# count = 0
+# for i in range(1, n+1):
+#     if not visited[i]:
+#         bfs(graph, i, visited)
+#         count += 1
+# print(count)
+
+# from collections import deque
+# import copy
+
+# n, m = map(int, input().split())
+# graph = []
+# for _ in range(n):
+#     graph.append(list(map(int, input().split())))
+    
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# def bfs():
+#     global answer
+#     queue = deque()
+#     tmp_graph = copy.deepcopy(graph)
+#     for i in range(n):
+#         for j in range(m):
+#             if tmp_graph[i][j] == 2:
+#                 queue.append((i, j))
+                
+#     while queue:
+#         x, y =queue.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if nx < 0 or ny < 0 or nx >= n or ny >= m:
+#                 continue
+#             if tmp_graph[nx][ny] == 1:
+#                 continue
+#             if tmp_graph[nx][ny] == 0:
+#                 tmp_graph[nx][ny] = 2
+#                 queue.append((nx, ny))
+                
+#     cnt = 0
+    
+#     for i in range(n):
+#         cnt += tmp_graph[i].count(0)
+    
+#     answer = max(answer, cnt)
+
+# def make_wall(cnt):
+#     if cnt == 3:
+#         bfs()
+#         return
+        
+#     for i in range(n):
+#         for j in range(m):
+#             if graph[i][j] == 0:
+#                 graph[i][j] = 1
+#                 make_wall(cnt+1)
+#                 graph[i][j] = 0
+                
+# answer = 0
+# make_wall(0)
+# print(answer)
+
+
+# from collections import deque
+
+# dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+# dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+# def bfs(x, y):
+#     queue = deque()
+#     queue.append((x,y))
+#     graph[x][y] = 0
+#     while queue:
+#         x, y = queue.popleft()
+#         for i in range(8):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if nx < 0 or ny < 0 or nx >= h or ny >= w:
+#                 continue
+#             if graph[nx][ny] == 1:
+#                 graph[nx][ny] = 0
+#                 queue.append((nx, ny))
+                
+# while True:
+#     w, h = map(int, input().split())
+#     if w == 0 and h == 0:
+#         break
+#     graph = []
+#     count = 0
+#     for _ in range(h):
+#         graph.append(list(map(int, input().split())))
+        
+#     for i in range(h):
+#         for j in range(w):
+#             if graph[i][j] == 1:
+#                 bfs(i, j)
+#                 count += 1
+#     print(count)
+
+
+from collections import deque
+import copy
 
 n, m = map(int, input().split())
+graph = []
+for _  in range(n):
+    graph.append(list(map(int, input().split())))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+answer = 0
+def bfs():
+  count = 0
+  global answer
+  queue = deque()
+  tmp_graph = copy.deepcopy(graph)
+  for i in range(n):
+    for j in range(m):
+      if tmp_graph[i][j] == 2:
+        queue.append((i, j))
 
-graph = [[] for _ in range(n+1)]
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    
-for i in range(n+1):
-    graph[i].sort()
-    
-print(graph)
+  while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx < 0 or ny < 0 or nx >= n or ny >=m:
+        continue
+      if tmp_graph[nx][ny] == 0:
+        tmp_graph[nx][ny] = 2
+        queue.append((nx, ny))
+
+  for i in range(n):
+    count += tmp_graph[i].count(0)
+    answer = max(answer, count)
+  return answer
+  
+def wall(cnt):
+  if cnt == 3:
+    bfs()
+    return
+
+  for i in range(n):
+    for j in range(m):
+      if graph[i][j] == 0:
+        graph[i][j] = 1
+        wall(cnt+1)
+        graph[i][j] = 0
+wall(0)
+print(bfs())
