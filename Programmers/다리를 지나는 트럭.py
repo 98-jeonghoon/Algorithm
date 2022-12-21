78f1c09af0c0568c09b47a8b0ccd2b4563d470e6
98-jeonghoon <bjh4395@naver.com>
2022-12-21 18:15:59 +0900

    프로그래머스 문제 풀이

---
+++ Programmers/다리를 지나는 트럭.py
@@ -0,0 +1,20 @@
+def solution(bridge_length, weight, truck_weights):
+    from collections import deque
+    answer = 0
+    if sum(truck_weights) <= weight:
+        return bridge_length + len(truck_weights)
+    bridge = [0 for _ in range(bridge_length)]
+    while bridge:
+        answer += 1
+        bridge.pop(0)
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:            
+                t = truck_weights.pop(0)
+                bridge.append(t)
+            else:
+                bridge.append(0)
+    return answer
+
+
+
+print(solution(2, 10, [7,4,5,6]))
