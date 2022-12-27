298e1e2b5e31a09de4c485ab12d60ce8e1dda82a
98-jeonghoon <bjh4395@naver.com>
2022-12-27 17:19:51 +0900

    프로그래머스 문제 풀이

---
+++ Programmers/쿼드압축 후 개수 세기.py
@@ -0,0 +1,24 @@
+def solution(arr):
+    answer = [0, 0]
+    length = len(arr)
+    
+    def quad_tree(x, y, length):
+        init = arr[x][y]
+
+        for i in range(x, x+length):
+            for j in range(y, y+length):
+                if arr[i][j] != init:
+                    length = length // 2
+                    quad_tree(x, y, length)
+                    quad_tree(x, y + length, length)
+                    quad_tree(x + length, y, length)
+                    quad_tree(x + length, y + length, length)
+                    return
+        answer[init] += 1
+    
+    quad_tree(0, 0, length)
+    
+    return answer
+
+
+print(solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]))
