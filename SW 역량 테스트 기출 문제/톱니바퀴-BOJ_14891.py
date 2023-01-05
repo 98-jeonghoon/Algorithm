77c73b31c67af935e6a3f456c3221dd36e20a59e
98-jeonghoon <bjh4395@naver.com>
2023-01-05 18:29:42 +0900

    삼성 기출문제 (톱니바퀴)

---
+++ SW 역량 테스트 기출 문제/톱니바퀴-BOJ_14891.py
@@ -0,0 +1,33 @@
+from collections import deque
+gears = {}
+for i in range(1, 5):
+    gears[i] = deque(list(map(int, input())))
+
+def check_right(start, dirs):
+    if start > 4 or gears[start-1][2] == gears[start][6]:
+        return
+    
+    if gears[start-1][2] != gears[start][6]:
+        check_right(start + 1, -dirs)
+        gears[start].rotate(dirs)
+
+def check_left(start, dirs):
+    if start < 1 or gears[start][2] == gears[start+1][6]:
+        return
+    
+    if gears[start+1][6] != gears[start][2]:
+        check_left(start - 1, -dirs)
+        gears[start].rotate(dirs)
+
+t = int(input())
+for _ in range(t):
+    num, dirs = map(int, input().split())
+    check_right(num+1, -dirs)
+    check_left(num-1, -dirs)
+    gears[num].rotate(dirs)
+
+answer = 0
+for i in range(4):
+    answer += (2**i) * gears[i+1][0]
+
+print(answer)
