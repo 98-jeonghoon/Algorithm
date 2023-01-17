d96d5959d548c887daf78917f05962937d2be95a
98-jeonghoon <bjh4395@naver.com>
2023-01-17 15:14:44 +0900

    프로그래머스 문제풀이

---
+++ Programmers/과일 장수.py
@@ -0,0 +1,11 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    for i in range(0, len(score), m):
+        tmp = score[i:i+m]
+
+        if len(tmp) == m:
+            answer += min(tmp) * m
+    return answer
+
+print(solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]))
