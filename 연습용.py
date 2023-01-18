94131878a41b64b57b06956f1a3e62bdc8c278af
98-jeonghoon <bjh4395@naver.com>
2023-01-18 09:21:46 +0900

    프로그래머스 복습

--- 연습용.py
+++ 연습용.py
@@ -622,3 +622,37 @@
 #         if not visited[i]:
 #             dfs(graph, i, visited)
     
+## 프로그래머스 lv3 정수 삼각형
+
+# def solution(triangle):
+#     dp = [[0] * len(triangle) for _ in range(len(triangle))]
+#     dp[0][0] = triangle[0][0]
+
+#     for i in range(0, len(triangle) - 1):
+#         for j in range(len(triangle[i])):
+#             dp[i+1][j] = max(dp[i+1][j], dp[i][j] + triangle[i+1][j])
+#             dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j] + triangle[i+1][j+1])
+#     return max(dp[-1])
+
+
+# print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
+
+
+## 프로그래머스 lv3 네트워크
+
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    for i in range(n):
+        if visited[i] == False:
+            dfs(n, computers, visited, i)
+            answer += 1
+    return answer
+def dfs(n, computers, visited, start):
+    visited[start] = True
+    for i in range(n):
+        if i != start and computers[start][i] == 1:
+            if not visited[i]:
+                dfs(n, computers, visited, i)
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
