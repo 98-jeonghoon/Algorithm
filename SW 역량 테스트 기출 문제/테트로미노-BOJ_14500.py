af394cf984bea7e23d89804ee63f1484e4d2a608
98-jeonghoon <bjh4395@naver.com>
2023-02-10 04:45:47 +0900

    백준문제풀이

--- SW 역량 테스트 기출 문제/테트로미노-BOJ_14500.py
+++ SW 역량 테스트 기출 문제/테트로미노-BOJ_14500.py
@@ -5,42 +5,46 @@ visited = [[False] * m for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-max_num = -1e9
+answer = -1e9
 
-def dfs(x, y, cnt, tmp):
-       global max_num
-       if cnt == 4:
-              max_num = max(max_num, tmp)
-              return
-       
-       for i in range(4):
-              nx = x + dx[i]
-              ny = y + dy[i]
-              if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-                     visited[nx][ny] = True
-                     dfs(nx, ny,cnt + 1 ,tmp + graph[nx][ny])
-                     visited[nx][ny] = False
-              
-def pink(x, y):
-       global max_num
-       for i in range(4):
-              tmp = graph[x][y]
-              for j in range(3):
-                     direction = (i + j) % 4
-                     nx = x + dx[direction]
-                     ny = y + dy[direction]
-                     if not (0 <= nx < n and 0 <= ny < m):
-                            tmp = 0
-                            break
-                     tmp += graph[nx][ny]
-              max_num = max(max_num, tmp)
-                            
-                     
+def dfs(depth, x, y, total):
+    global answer
+    
+    if depth == 4:
+        answer = max(answer, total)
+        return
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            if visited[nx][ny] == True:
+                continue
+            visited[nx][ny] = True
+            dfs(depth + 1, nx, ny, total + graph[nx][ny])
+            visited[nx][ny] = False
+            
+def purple(x, y):
+    global answer
+    for i in range(4):
+        tmp = graph[x][y]
+        for j in range(3):
+            d = (i + j) % 4
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                tmp = 0
+                break
+            else:
+                tmp += graph[nx][ny]
+        answer = max(answer, tmp)
+            
+
 for i in range(n):
-       for j in range(m):
-              visited[i][j] = True
-              dfs(i, j, 1, graph[i][j])
-              visited[i][j] = False
-              pink(i, j)
+    for j in range(m):
+        visited[i][j] = True
+        dfs(1, i, j, graph[i][j])
+        visited[i][j] = False
+        purple(i, j)
 
-print(max_num)
+print(answer)
