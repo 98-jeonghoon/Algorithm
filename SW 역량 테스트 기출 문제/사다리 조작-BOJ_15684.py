5dce70b5f4384337a5a99f284a23380b6ae37a48
98-jeonghoon <bjh4395@naver.com>
2023-02-10 04:46:22 +0900

    삼성 기출 문제 풀이

--- SW 역량 테스트 기출 문제/사다리 조작-BOJ_15684.py
+++ SW 역량 테스트 기출 문제/사다리 조작-BOJ_15684.py
@@ -1,18 +1,15 @@
 n, m, h = map(int, input().split())
+graph = [[0] * n for _ in range(h)]
 
-graph = [[0] *n for _ in range(h)]
-
-for _ in range(m):
+for i in range(m):
     a, b = map(int, input().split())
-    graph[a-1][b-1] = 1
-    
-answer = 4
+    graph[a - 1][b - 1] = 1
 
 def check():
     for i in range(n):
         temp = i
         for j in range(h):
-            if graph[j][temp]:
+            if graph[j][temp] == 1:
                 temp += 1
             elif temp > 0 and graph[j][temp - 1]:
                 temp -= 1
@@ -20,30 +17,29 @@ def check():
             return False
     return True
 
-
-def dfs(depth, x, y):
+answer = 4
+def backtracking(depth, x, y):
     global answer
-    
-    if answer <= depth:
-        return
     if check():
         answer = min(answer, depth)
+        
     if depth == 3:
         return
+    
     for i in range(x, h):
         if i == x:
             k = y
         else:
             k = 0
-        for j in range(k, n - 1):
+        for j in range(k, n-1):
             if graph[i][j] == 1:
                 continue
             graph[i][j] = 1
-            dfs(depth + 1, i, j + 2)
+            backtracking(depth + 1, i, j + 2)
             graph[i][j] = 0
 
-dfs(0, 0, 0)
+backtracking(0, 0, 0)
 if answer <= 3:
     print(answer)
 else:
-    print(-1)                
+    print(-1)
