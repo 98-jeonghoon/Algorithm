02ca7dc1ec8a2874b93febbf089c13dd4b1d0749
98-jeonghoon <bjh4395@naver.com>
2023-02-23 18:44:19 +0900

    SW 역량 테스트 문제풀이

--- SW 역량 테스트 기출 문제/드래곤 커브-BOJ_15685.py
+++ SW 역량 테스트 기출 문제/드래곤 커브-BOJ_15685.py
@@ -1,31 +1,35 @@
 n = int(input())
+graph = [[0] * 101 for _ in range(101)]
+
+# 우 상 좌 하
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
 
-arr = [[0] * 100 for _ in range(100)]
-
-for i in range(n):
-    x, y, d, g = map(int, input().split())
-    arr[x][y] = 1
+def dragon_curved(x, y, d, g):
+    graph[x][y] = 1
     
     move = [d]
     
     for _ in range(g):
         tmp = []
         for i in range(len(move)):
-            tmp.append((move[-i -1] + 1) % 4)
+            tmp.append((move[-i - 1] + 1) % 4)
         move.extend(tmp)
-        
+    
     for i in move:
         nx = x + dx[i]
         ny = y + dy[i]
-        arr[nx][ny] = 1
+        graph[nx][ny] = 1
         x, y = nx, ny
-        
+
+for _ in range(n):
+    x, y, d, g = map(int, input().split())
+    dragon_curved(x, y, d, g)
+
 answer = 0
+
 for i in range(100):
     for j in range(100):
-        if arr[i][j] and arr[i + 1][j] and arr[i][j + 1] and arr[i + 1][j + 1]:
+        if graph[i][j] and graph[i + 1][j] and graph[i][j + 1] and graph[i + 1][j + 1]:
             answer += 1
-            
 print(answer)
