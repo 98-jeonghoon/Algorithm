8f56339dba0b4443d6af0615593cbde9cc5d4d23
98-jeonghoon <bjh4395@naver.com>
2023-03-24 18:49:50 +0900

    삼성 문제 풀이

--- SW 역량 테스트 기출 문제/마법사 상어와 비바라기-BOJ_21610.py
+++ SW 역량 테스트 기출 문제/마법사 상어와 비바라기-BOJ_21610.py
@@ -1,98 +1,62 @@
-## 1트 실패 (쓸데 없는 copy + 구름을 동시에 옮겨야 하는데 하나씩 옮기고 계산해서 틀림)
-
-# import copy
-# n, m = map(int, input().split())
-# graph = [list(map(int, input().split())) for _ in range(n)]
-# visited = [[False] * n for _ in range(n)]
-# dx = [0, -1, -1, -1, 0, 1, 1, 1]
-# dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-# check_move = [1, 3, 5, 7]
-# cloud = [(n - 2, 0), (n - 2, 1), (n - 1, 0), (n - 1, 1)]
-
-# cmd = []
-# for _ in range(m):
-#     d, s = map(int, input().split())
-#     cmd.append((d - 1, s))
-
-# for _ in range(m):
-#     d, s = cmd.pop(0)
-#     tmp_visited = copy.deepcopy(visited)
-#     while cloud:
-#         x, y = cloud.pop(0)
-#         nx = (x + s * dx[d]) % n
-#         ny = (y + s * dy[d]) % n
-#         graph[nx][ny] += 1
-#         count = 0
-#         for i in check_move:
-#             check_nx = nx + dx[i]
-#             check_ny = ny + dy[i]
-#             if check_nx < 0 or check_ny < 0 or check_nx >= n or check_ny >= n:
-#                 continue
-#             if graph[check_nx][check_ny] == 0:
-#                 continue
-#             count += 1
-#         graph[nx][ny] += count
-#         tmp_visited[nx][ny] = True
-
-#     for i in range(n):
-#         for j in range(n):
-#             if tmp_visited[i][j] ==False and graph[i][j] >= 2:
-#                 graph[i][j] -= 2
-#                 cloud.append((i, j))
-        
-        
-# for i in graph:
-#     print(i)
-
-# 2트
-
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-dx = [0, -1, -1, -1, 0, 1, 1, 1]
-dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-check_move = [1, 3, 5, 7]
-cloud = [(n - 2, 0), (n - 2, 1), (n - 1, 0), (n - 1, 1)]
 
-cmd = []
+command = []
 for _ in range(m):
     d, s = map(int, input().split())
-    cmd.append((d - 1, s))
+    command.append((d - 1, s))
 
+dx = [0, -1, -1, -1, 0, 1, 1, 1]
+dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 
-for _ in range(m):
-    d, s = cmd.pop(0)
-    new_cloud = []
-    visited = [[False] * n for _ in range(n)]
+cloud = [(n - 1, 0), (n - 1, 1), (n - 2, 0), (n - 2, 1)]
+tmp = []
+def move_cloud(d, s):
+    global cloud
     for _ in range(len(cloud)):
         x, y = cloud.pop(0)
-        nx = (x + s * dx[d]) % n
-        ny = (y + s * dy[d]) % n
-        new_cloud.append((nx, ny))
-        
-    for i in range(len(new_cloud)):
-        x, y = new_cloud[i]
-        graph[x][y] += 1
-        visited[x][y] = True
-        
-
+        nx = (x + dx[d] * s) % n
+        ny = (y + dy[d] * s) % n
+        cloud.append((nx, ny))
+        tmp.append((nx, ny))
     
-    for i in range(len(new_cloud)):
-        x, y = new_cloud[i]
+def rained():
+    for i in range(len(cloud)):
+        x, y = cloud[i]
+        graph[x][y] += 1
+
+def step_3():
+    while cloud:
+        x, y = cloud.pop()
         count = 0
-        for i in check_move:
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] >= 1:
-                count += 1
+        for d in [1, 3, 5, 7]:
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] != 0:
+                    count += 1
         graph[x][y] += count
 
-    for i in range(n):
-        for j in range(n):
-            if visited[i][j] == False and graph[i][j] >= 2:
-                graph[i][j] -= 2
-                cloud.append((i, j))
-        
-answer = 0   
-for i in graph:
-    answer += sum(i)
+def append_cloud():
+    for x in range(n):
+        for y in range(n):
+            if graph[x][y] >= 2 and (x, y) not in tmp:
+                graph[x][y] -= 2
+                cloud.append((x, y))
+    
+    tmp.clear()
+
+for _ in range(m):
+    d, s = command.pop(0)
+    move_cloud(d, s)
+    rained()
+    step_3()
+    append_cloud()
+
+
+answer = 0
+for i in range(n):
+    for j in range(n):
+        answer += graph[i][j]
+
 print(answer)
