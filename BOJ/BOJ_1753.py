9340dc0d79b52494958a31d449eebb63df4d0604
98-jeonghoon <bjh4395@naver.com>
2023-03-28 18:37:14 +0900

    다익스트라 문제 풀이

--- BOJ/BOJ_1753.py
+++ BOJ/BOJ_1753.py
@@ -1,15 +1,11 @@
-# 도시개수 n, 통로의 개수 m, 보내고자 하는 도시 c
-
-
 import heapq
+n, m = map(int, input().split())
+start = int(input())
 INF = int(1e9)
-n, m, start = map(int, input().split())
-
 graph = [[] * (n + 1) for _ in range(n + 1)]
 distance = [INF] * (n + 1)
 
 for _ in range(m):
-    # a 에서 b로 가는데 c만큼 소요된다.
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
 
@@ -29,13 +25,9 @@ def dijkstra(start):
                 heapq.heappush(queue, (cost, i[0]))
 
 dijkstra(start)
-print(distance)
-count = 0
-max_distance = 0
 
-for d in distance:
-    if d != INF:
-        count += 1
-        max_distance = max(max_distance, d)
-
-print(count - 1, max_distance)
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print('INF')
+    else:
+        print(distance[i])
--- BOJ/BOJ_1916.py
+++ BOJ/BOJ_1916.py
@@ -1,19 +1,19 @@
-# 도시개수 n, 통로의 개수 m, 보내고자 하는 도시 c
+n = int(input())
+m = int(input())
 
-
-import heapq
 INF = int(1e9)
-n, m, start = map(int, input().split())
 
 graph = [[] * (n + 1) for _ in range(n + 1)]
 distance = [INF] * (n + 1)
 
 for _ in range(m):
-    # a 에서 b로 가는데 c만큼 소요된다.
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
 
+start, destination = map(int, input().split())
+
 def dijkstra(start):
+    import heapq
     queue = []
     heapq.heappush(queue, (0, start))
     distance[start] = 0
@@ -29,13 +29,5 @@ def dijkstra(start):
                 heapq.heappush(queue, (cost, i[0]))
 
 dijkstra(start)
-print(distance)
-count = 0
-max_distance = 0
 
-for d in distance:
-    if d != INF:
-        count += 1
-        max_distance = max(max_distance, d)
-
-print(count - 1, max_distance)
+print(distance[destination])
