f2c760b7323fa102bce77f3d606c7ceb2db30235
98-jeonghoon <bjh4395@naver.com>
2023-04-23 13:49:10 +0900

    프로그래머스 다시 풀기

--- Programmers/두 큐 합 같게 만들기.py
+++ Programmers/두 큐 합 같게 만들기.py
@@ -23,37 +23,35 @@
 # print(solution([102, 1], [101, 100]))
 
 
-from collections import deque
+# from collections import deque
 
-def solution(queue1, queue2):
-    answer = 0
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    sum_1 = sum(queue1)
-    sum_2 = sum(queue2)
-    limit = len(queue1) * 3
-    total = sum_1 + sum_2
-    if total % 2 != 0:
-        return -1
-    while True:
-        if sum_1 > sum_2:
-            now = queue1.popleft()
-            queue2.append(now)
-            sum_1 -= now
-            sum_2 += now
-            answer += 1
-        elif sum_2 > sum_1:
-            now = queue2.popleft()
-            queue1.append(now)
-            sum_1 += now
-            sum_2 -= now
-            answer += 1
-        else:
-            break
-        if answer == limit:
-            answer = -1
-            break
-    return answer
+# def solution(queue1, queue2):
+#     answer = 0
+#     queue1 = deque(queue1)
+#     queue2 = deque(queue2)
+#     sum_1 = sum(queue1)
+#     sum_2 = sum(queue2)
+#     limit = len(queue1) * 3
+#     total = sum_1 + sum_2
+#     if total % 2 != 0:
+#         return -1
+#     while True:
+#         if sum_1 > sum_2:
+#             now = queue1.popleft()
+#             queue2.append(now)
+#             sum_1 -= now
+#             sum_2 += now
+#             answer += 1
+#         elif sum_2 > sum_1:
+#             now = queue2.popleft()
+#             queue1.append(now)
+#             sum_1 += now
+#             sum_2 -= now
+#             answer += 1
+#         else:
+#             break
+#         if answer == limit:
+#             answer = -1
+#             break
+#     return answer
         
-
-print(solution([3, 2, 7, 2], [4, 6, 5, 1]))
