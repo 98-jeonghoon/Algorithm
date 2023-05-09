fd58a0d3c910143c81ac1c9e144bdfe41d71780c
98-jeonghoon <bjh4395@naver.com>
2023-05-09 15:40:29 +0900

    시뮬레이션 문제풀이

--- SW 역량 테스트 기출 문제/코드트리 빵.py
+++ SW 역량 테스트 기출 문제/코드트리 빵.py
@@ -1,75 +1,85 @@
 from collections import deque
-
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-
-visited = [[False] * n for _ in range(n)]
-dist = [[0] * n for _ in range(n)]
-
-dx = [-1, 0, 0, 1]
-dy = [0, -1, 1, 0]
-people = [(-1, -1)] * m
 store = []
+people_pos = [(-1, -1)] * m
+
 for _ in range(m):
     x, y = map(int, input().split())
     store.append((x - 1, y - 1))
-    
-def ititialize_visited_dist():
-    for i in range(n):
-        for j in range(n):
-            visited[i][j] = False
-            dist[i][j] = 0
 
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+# 방향 우선순위 설정 (상 좌 우 하)
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
 
+
+# 편의점, 베이스캠프를 지날수있는지 확인하기 위하여 배열선언
+check_visited = [[False] * n for _ in range(n)]
+
+# 최단경로를 구하기 위한 bfs
 def bfs(x, y):
-    ititialize_visited_dist()
+    visited = [[False] * n for _ in range(n)]
+    dist = [[-1] * n for _ in range(n)]
     queue = deque()
     queue.append((x, y))
     visited[x][y] = True
-    
+    dist[x][y] = 0
+
     while queue:
         x, y = queue.popleft()
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
-            if in_range(nx, ny) and visited[nx][ny] == False and graph[nx][ny] != 2:
-                dist[nx][ny] = dist[x][y] + 1
-                visited[nx][ny] = True
-                queue.append((nx, ny))
+            # 격자를 벗어나지 않고, 방문하지 않았으면
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == False:
+                # 편의점, 베이스캠프를 경유할수 있다면
+                if check_visited[nx][ny] == False:
+                    queue.append((nx, ny))
+                    visited[nx][ny] = True
+                    dist[nx][ny] = dist[x][y] + 1
+    return dist, visited
+
+
+# 시뮬레이션이 종료되는지 확인하는 함수
+def check():
+    for i in range(m):
+        # 만약 편의점 좌표랑 사람의 좌표가 하나라도 다르면 False 리턴
+        if store[i] != people_pos[i]:
+            return False
+    # 모두 동일하다면 True 리턴
+    else:
+        return True
+    
 
 time = 0
-
 def simulate():
     for i in range(m):
-        if people[i] == (-1, -1) or people[i] == store[i]:
+        if people_pos[i] == (-1, -1) or people_pos[i] == store[i]:
             continue
-        
-        bfs(store[i][0], store[i][1])
-        now_x, now_y = people[i]
+        dist, visited = bfs(store[i][0], store[i][1])
+        now_x, now_y = people_pos[i]
         min_value = 1e9
         min_x, min_y = -1, -1
-        
+
         for d in range(4):
             nx = now_x + dx[d]
             ny = now_y + dy[d]
-            if in_range(nx, ny) and visited[nx][ny] == True and min_value > dist[nx][ny]:
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == True and check_visited[nx][ny] == False and min_value > dist[nx][ny]:
                 min_value = dist[nx][ny]
                 min_x, min_y = nx, ny
-        people[i] = (min_x, min_y)
-        
+
+        people_pos[i] = (min_x, min_y)
+
+    
     for i in range(m):
-        if people[i] == store[i]:
-            nx, ny = people[i]
-            graph[nx][ny] = 2
+        if people_pos[i] == store[i]:
+            check_x, check_y = people_pos[i]
+            check_visited[check_x][check_y] = True
     
-    
-    if time > m:
+    if time > m :
         return
     
-    bfs(store[time - 1][0], store[time - 1][1])
-    
+    dist, visited = bfs(store[time - 1][0], store[time - 1][1])
     min_value = 1e9
     min_x, min_y = -1, -1
     for x in range(n):
@@ -77,20 +87,13 @@ def simulate():
             if visited[x][y] == True and graph[x][y] == 1 and min_value > dist[x][y]:
                 min_value = dist[x][y]
                 min_x, min_y = x, y
-    people[time - 1] = (min_x, min_y)
-    graph[min_x][min_y] = 2
-    
-def end():
-    for i in range(m):
-        if people[i] != store[i]:
-            return False
-        
-    return True
-
+    people_pos[time - 1] = (min_x, min_y)
+    check_visited[min_x][min_y] = True
 while True:
     time += 1
     simulate()
-    if end():
+    if check():
         break
 
+    
 print(time)
