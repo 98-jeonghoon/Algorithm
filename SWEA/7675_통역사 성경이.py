fc103bf29d0a1a6a4d474ae1cd7b2567b0e871a5
98-jeonghoon <bjh4395@naver.com>
2023-05-15 18:23:58 +0900

    알고리즘 문제풀이

--- SWEA/7675_통역사 성경이.py
+++ SWEA/7675_통역사 성경이.py
@@ -1,50 +1,8 @@
-# n, c = map(int, input().split())
-# arr = []
-# for _ in range(n):
-#     a = int(input())
-#     arr.append(a)
-# arr.sort()
-
-# start = 1
-# end = arr[-1] - arr[0]
-# answer = 0
-# def binary_search(arr, start, end):
-#     global answer
-#     while start <= end:
-#         mid = (start + end) // 2
-#         current = arr[0]
-#         count = 1
-#         for i in range(1, len(arr)):
-#             if arr[i] >= current + mid:
-#                 count += 1
-#                 current = arr[i]
-        
-#         if count >= c:
-#             start = mid + 1
-#             answer = mid
-#         else:
-#             end = mid - 1
-            
-# binary_search(arr, start, end)
-# print(answer)
-
-# t = int(input())
-# for test in range(1, t + 1):
-#     s = input()
-#     # len_ = len(s)
-#     lose_cnt = s.count('x')
-#     answer = ''
-
-#     if lose_cnt >= 8:
-#         answer = 'NO'
-#     else:
-#         answer = 'YES'
-    
-#     print(f'#{test} {answer}')
-
 import string
 
+# 대문자가 들어있는 배열선언
 upper_case = list(string.ascii_uppercase)
+# 소문자가 들어있는 배열선언
 lower_case = list(string.ascii_lowercase)
 
 def is_name(word):
@@ -66,23 +24,37 @@ t = int(input())
 for test in range(1, t + 1):
     n = int(input())
     s = input()
+    # 각 문장을 담을 배열선언
     tmp = []
+    # 문자을 자르기위한 check_idx 값 설정
     check_idx = 0
+
     for i in range(len(s)):
+        # 구두점을 만난다면
         if s[i] == '!' or s[i] == '.' or s[i] == '?':
+            # 해당 문장까지 tmp 배열에 넣어주고
             tmp.append(s[check_idx:i])
+            # 구두점과 공백을 건너뛰기 위하여 check_idx를 2더해줌
             check_idx = i + 2
     
+    # 문장만큼 answer 배열선언
     answer = [0] * len(tmp)
 
+    # 문장을 돌면서
     for idx in range(len(tmp)):
+        # 문장을 단어로 쪼개서 배열에 담음
         word_arr = tmp[idx].split(' ')
+        # 이름이 몇개인지 체크하기 위한 cnt 변수 선언
         cnt = 0
+        # 단어를 돌면서
         for word in word_arr:
+            # 해당 단어가 이름인지 체크한다
             if is_name(word):
+                # 이름이면 cnt를 1올려준다
                 cnt += 1
+        # 이름의 개수를 answer 배열에 기록해준다
         answer[idx] = cnt
     
+    # 출력
     print(f'#{test} ', end='')
     print(*answer)
-        
